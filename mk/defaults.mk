10e9348d798d9a683c966480faca00cabc531dd5
Ian Moffett <ian@osmora.org>
2025-12-21 16:48:51 -0500

    initial commit
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ mk/defaults.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2025, Ian Moffett
+# Provided under the BSD-3 clause.
+#
+
+ARCH = x86_64
+QEMU = qemu-system-$(ARCH)
+
+CC = $(shell pwd)/var/cc/gcc/bin/$(ARCH)-pc-osmora-gcc
+LD = $(shell pwd)/var/cc/toolchain/build-binutils/bin/$(ARCH)-pc-osmora-ld
+
+SYS_CFLAGS =			\
+	-nostdlib			\
+	-nostdinc			\
+	-ffreestanding		\
+	-fexceptions		\
+	--std=gnu11			\
+	-mcmodel=kernel		\
+	-Wno-attributes
+
+ifeq ($(ARCH),x86_64)
+	SYS_CFLAGS += 		\
+		-mno-sse		\
+		-mno-sse2		\
+		-mno-sse3		\
+		-mno-avx		\
+		-mno-avx2		\
+		-mno-80387		\
+		-mno-3dnow
+endif
