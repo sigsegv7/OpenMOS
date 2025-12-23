a92a1b8e6d7537d37a5e9fb3b56680b3d5a1542b
Ian Moffett <ian@osmora.org>
2025-12-22 20:01:04 -0500

    usr: sdk: Add units.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ usr/sdk/inc/sdk/units.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#ifndef _SDK_UNITS_H_
+#define _SDK_UNITS_H_ 1
+
+/* Storage size units */
+#define UNIT_GIB 0x40000000
+#define UNIT_MIB 0x100000
+
+#endif  /* !_SDK_UNITS_H_ */
