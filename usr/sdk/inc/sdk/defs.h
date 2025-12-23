0ca11cce7f3660a4b92441c39c30b24866a210c6
Ian Moffett <ian@osmora.org>
2025-12-22 20:01:18 -0500

    usr: sdk: Add sdk/defs.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ usr/sdk/inc/sdk/defs.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#ifndef _SDK_DEFS_H_
+#define _SDK_DEFS_H_ 1
+
+#define ASM             __asm__ __volatile__
+#define ATTR(x)         __attribute__((x))
+#define ALIGN(n)        ATTR(aligned((n)))
+#define NO_RETURN       ATTR(no_return)
+#define ALWAYS_INLINE   ATTR(always_inline)
+
+#endif  /* !_SDK_DEFS_H_ */
